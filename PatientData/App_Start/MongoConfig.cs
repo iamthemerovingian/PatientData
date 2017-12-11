76f06623caae81f5e042f8c2feac32fdb9909389
SURFACEPRO\iamth <iamthemerovingian@hotmail.com>
2017-12-11 23:05:50 +0700

    Started building a web api using mongodb as a datastore.

---
+++ PatientData/App_Start/MongoConfig.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Driver;
+using PatientData.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PatientData
+{
+    public class MongoConfig
+    {
+        public static void Seed()
+        {
+            var patients = PatientDb.Open();
+
+            if (!patients.AsQueryable().Any(p => p.Name == "Scott"))
+            {
+                var data = new List<Patient>
+                {
+                    new Patient { Name = "Scott", Ailments =  new List<Ailment> { new Ailment { Name = "Bald"} }, Medications = new List<Medication> { new Medication { Name = "Mioxil", Dosage = "100mg" } } },
+                    new Patient { Name = "Mili", Ailments =  new List<Ailment> { new Ailment { Name = "Crazy"} }, Medications = new List<Medication> { new Medication { Name = "Symbicort", Dosage = "100mg" } } },
+                    new Patient { Name = "Baldie", Ailments =  new List<Ailment> { new Ailment { Name = "Balding"} }, Medications = new List<Medication> { new Medication { Name = "Rogaine", Dosage = "100mg" } } }
+                };
+
+                patients.InsertMany(data);
+            }
+        }
+    }
+}
---
+++ PatientData/Controllers/PatientController.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Driver;
+using PatientData.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace PatientData.Controllers
+{ 
+    public class PatientController : ApiController
+    {
+        private readonly IMongoCollection<Patient> ThePatientCollection;
+
+        public PatientController()
+        {
+            ThePatientCollection = PatientDb.Open();
+        }
+
+        public async Task<IHttpActionResult> Get()
+        {
+            return Ok(await ThePatientCollection.Find(_ => true).ToListAsync());
+        }
+    }
+}
